eff3db97e034d5ca395b129f9bc66b9491d96b5f
Robbin <robbindrent@gmail.com>
2022-11-24 13:45:54 +0100

    Alles tot en met delete en edit

---
+++ src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/model/Film.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.ch10.robbin.dvdLibrary.model;/**
+ * @author Robbin Drent <r.v.drent@st.hanze.nl>
+ *
+ * Dit is wat het programma doet.
+ */
+public class Film {
+}
---
+++ src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/repository/FilmRepository.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.ch10.robbin.dvdLibrary.repository;/**
+ * @author Robbin Drent <r.v.drent@st.hanze.nl>
+ *
+ * Dit is wat het programma doet.
+ */
+public class FilmRepository {
+}

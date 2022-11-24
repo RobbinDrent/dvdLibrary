37d432246fd00302217974176ac737e4d1462b6b
Robbin <robbindrent@gmail.com>
2022-11-24 13:50:46 +0100

    nog een poging

--- src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/model/Film.java
+++ src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/model/Film.java
@@ -1,7 +1,46 @@
-package nl.miwgroningen.ch10.robbin.dvdLibrary.model;/**
+package nl.miwgroningen.ch10.robbin.dvdLibrary.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
  * @author Robbin Drent <r.v.drent@st.hanze.nl>
- *
+ * <p>
  * Dit is wat het programma doet.
  */
+
+@Entity
 public class Film {
+
+    @Id
+    @GeneratedValue
+    private Long filmId;
+
+    private String title;
+    private String director;
+
+    public Long getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(Long filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
--- src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/repository/FilmRepository.java
+++ src/main/java/nl/miwgroningen/ch10/robbin/dvdLibrary/repository/FilmRepository.java
@@ -1,7 +1,12 @@
-package nl.miwgroningen.ch10.robbin.dvdLibrary.repository;/**
+package nl.miwgroningen.ch10.robbin.dvdLibrary.repository;
+
+import nl.miwgroningen.ch10.robbin.dvdLibrary.model.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
  * @author Robbin Drent <r.v.drent@st.hanze.nl>
- *
+ * <p>
  * Dit is wat het programma doet.
  */
-public class FilmRepository {
+public interface FilmRepository extends JpaRepository<Film, Long> {
 }

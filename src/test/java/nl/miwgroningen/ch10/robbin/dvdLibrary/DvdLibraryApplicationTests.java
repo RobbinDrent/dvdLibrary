0ef18b3fbf824041bab2e0b5a1ac1fe4e450e7a4
Robbin <robbindrent@gmail.com>
2022-11-24 12:00:35 +0100

    first commit

---
+++ src/test/java/nl/miwgroningen/ch10/robbin/dvdLibrary/DvdLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.ch10.robbin.dvdLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
